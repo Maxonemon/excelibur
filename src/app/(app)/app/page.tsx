087e1629bb4eff669637b42e6cd49bb7bce7ac7a
Maxonemon <110642351+Maxonemon@users.noreply.github.com>
2024-08-11 15:21:01 +0000

    second commit

--- src/app/(app)/app/page.tsx
+++ src/app/(app)/app/page.tsx
@@ -3,9 +3,7 @@ import { signOut } from "~/src/auth";
 import Chat from "~/src/components/chat";
 import getSession from "~/src/lib/getSession";
 
-export const Metadata = {
-  title: "app",
-};
+
 export default async function page() {
   const session = await getSession();
   const user = session?.user;
