5bce5e75d69cd0e0b976826215a0101d40623cb8
Maxonemon <110642351+Maxonemon@users.noreply.github.com>
2024-08-11 17:49:58 +0000

    fifth commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import nyxb from '@nyxb/eslint-config'
+// import nyxb from '@nyxb/eslint-config'
 
-export default nyxb(
-   {
-      formatters: true,
-      react: true,
-      tailwindcss: {
-         settings: {
-            callees: ['classnames', 'ny', 'cn'],
-            config: './apps/web/tailwind.config.ts',
-         },
-      },
-   },
-   {
-      rules: {},
-   },
-   {
-      ignores: ['**/dist/**', '**/node_modules/**', 'packages/cli/test/**'],
-   },
-)
+// export default nyxb(
+//    {
+//       formatters: true,
+//       react: true,
+//       tailwindcss: {
+//          settings: {
+//             callees: ['classnames', 'ny', 'cn'],
+//             config: './apps/web/tailwind.config.ts',
+//          },
+//       },
+//    },
+//    {
+//       rules: {},
+//    },
+//    {
+//       ignores: ['**/dist/**', '**/node_modules/**', 'packages/cli/test/**'],
+//    },
+// )
