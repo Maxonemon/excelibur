65149ef57b607ef58532897ad9d76d8299808f67
Maxonemon <110642351+Maxonemon@users.noreply.github.com>
2024-11-12 20:33:56 +0000

    Fix bug in chatbot logic

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
+import GoogleLogoIcon from "~/src/components/google-logo-icon";
 import { Button, buttonVariants } from "~/src/components/ui/button";
 import {
   Card,
@@ -107,21 +108,7 @@ export default function LoginPage() {
               {isGoogleLoading || isGitHubLoading ? (
                 <Loader2 className="mr-2 size-4 animate-spin" />
               ) : (
-                <svg
-                  className="mr-2 h-4 w-4"
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="google"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 488 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                  ></path>
-                </svg>
+                <GoogleLogoIcon className="mr-2 size-4" />
               )}
               Google
             </Button>
