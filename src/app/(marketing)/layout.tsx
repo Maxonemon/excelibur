c6aa288eb60f2e86aee5707ee8ff3201e99c0199
Maxonemon <110642351+Maxonemon@users.noreply.github.com>
2024-11-15 11:22:08 +0000

    redirect authenticated user

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,18 +1,25 @@
-import { SiteFooter } from '~/src/components/site-footer'
-import { SiteHeader } from '~/src/components/site-header'
+import { redirect } from "next/navigation";
+import { auth } from "~/src/auth";
+import { SiteFooter } from "~/src/components/site-footer";
+import { SiteHeader } from "~/src/components/site-header";
 
 interface MarketingLayoutProps {
-   children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default async function MarketingLayout({
-   children,
+  children,
 }: MarketingLayoutProps) {
-   return (
-      <>
-         <SiteHeader />
-         <main className="mx-auto flex-1 overflow-hidden">{children}</main>
-         <SiteFooter />
-      </>
-   )
+  const session = await auth();
+  const user = session?.user;
+  if (user) {
+    redirect("/app");
+  }
+  return (
+    <>
+      <SiteHeader />
+      <main className="mx-auto flex-1 overflow-hidden">{children}</main>
+      <SiteFooter />
+    </>
+  );
 }
